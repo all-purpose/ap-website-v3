f0d03dad4179533ec22667f257aa9372126a1a49
Dave <dave@Daves-MacBook-Pro-2.local>
2020-08-18 10:56:44 -0700

    adding contact page and ccontact content component for form

---
+++ src/components/contactPageContent/ContactPageContent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const ContactPageContent = props => {
+
+  return (
+    <div className="page-sections">
+      Put the content here Joseph! :)
+    </div>
+  )
+
+
+}
+
+export default ContactPageContent;
