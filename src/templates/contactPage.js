26c3116af019e0ee651714c47188bc0f36e64f30
Joseph Coombes <josephcoombes@Josephs-MacBook-Pro-3.local>
2020-08-20 08:39:48 -0700

    add scss map function and more spacing/type adjustments

--- src/templates/contactPage.js
+++ src/templates/contactPage.js
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from 'react';
-import { graphql } from 'gatsby';
-import Layout from '../components/layout/Layout';
-import PageHeaderGeneral from '../components/pageHeader/PageHeaderGeneral';
-import ContactPageContent from '../components/contactPageContent/ContactPageContent';
+import React, { useState, useEffect } from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout/Layout"
+import PageHeaderGeneral from "../components/pageHeader/PageHeaderGeneral"
+import ContactPageContent from "../components/contactPageContent/ContactPageContent"
 
 export const query = graphql`
-  query ContactPageQuery { 
+  query ContactPageQuery {
     prismic {
       allContact_pages {
         edges {
@@ -21,48 +21,41 @@ export const query = graphql`
       }
     }
   }
-`;
+`
 
-const ContactPage = props => {
-
-  const [selectedPalette, setSelectedPalette] = useState(null);
+const ContactPage = (props) => {
+  const [selectedPalette, setSelectedPalette] = useState(null)
 
   function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min
   }
 
-  // Run theme selection once on mounting  
-  useEffect(() => {    
-    const random = getRandomInt(1, 11); 
-    setSelectedPalette(`palette-${random}`);
-  }, []);
+  // Run theme selection once on mounting
+  useEffect(() => {
+    const random = getRandomInt(1, 11)
+    setSelectedPalette(`palette-${random}`)
+  }, [])
 
   const {
     _meta,
     page_title,
     page_description,
-  } = props.data.prismic.allContact_pages.edges[0].node;
+  } = props.data.prismic.allContact_pages.edges[0].node
 
-  const {
-    uid,
-    type
-  } = _meta;
+  const { uid, type } = _meta
 
   return (
     <Layout palette={selectedPalette} type={type} uid={uid}>
-      <PageHeaderGeneral 
-        title={page_title} 
-        description={page_description} 
-      />
-      <div className="page-sections">
-        <div className="container">
+      <PageHeaderGeneral title={page_title} description={page_description} />
+      <div className="page-sections apply-color-theme ">
+        <div className="container py-48 border-solid border-t">
           <ContactPageContent />
         </div>
       </div>
     </Layout>
-  );
+  )
 }
 
-export default ContactPage;
+export default ContactPage
