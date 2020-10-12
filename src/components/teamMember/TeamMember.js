d088f7be738b67b3e956b37ebfa27d39d74bedae
Dave <dave@Daves-MacBook-Pro-2.local>
2020-10-12 12:16:41 -0700

    add lazysizes package for lazy loading images issue 30

--- src/components/teamMember/TeamMember.js
+++ src/components/teamMember/TeamMember.js
@@ -1,11 +1,20 @@
 import React from "react"
 import { RichText } from "prismic-reactjs"
+import 'lazysizes';
 
 const TeamMember = ({ photo, name, specialties }) => {
+  let mobileUrl = '';
+
+  try {
+    mobileUrl = photo.Mobile.url
+  } catch (err) {
+    console.error(err);
+  }
+
   return (
     <div className="team-member">
       <div className="team-member-photo">
-        <img src={photo.url} alt={photo.alt} />
+        <img className="lazyload" data-lowsrc={mobileUrl} data-src={photo.url} alt={photo.alt} data-sizes="auto" />
       </div>
       <h2 className="team-member-name body-short-02 serif mt-4">{name}</h2>
       <div className="team-member-specialties body-01">
