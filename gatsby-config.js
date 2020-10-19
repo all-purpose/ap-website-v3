7d4bd3d369dd165167e4df4f920dd4da0428c238
Vivian Chen <vivianchen@ca.ibm.com>
2020-10-19 15:59:44 -0700

    careers page progress

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({
+require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
@@ -8,7 +8,7 @@ module.exports = {
     title: `All Purpose`,
     description: `Our mission is to help organizations positively impact people and the planet through the design and creation of human-centered experiences and products, and inspire others to do the same.`,
     author: `All Purpose`,
-    image: `/img/all-purpose_image.png`
+    image: `/img/all-purpose_image.png`,
   },
   plugins: [
     `gatsby-plugin-sitemap`,
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `UA-140430866-1`
-      }
+        trackingId: `UA-140430866-1`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
@@ -28,12 +28,12 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     {
-      resolve: 'gatsby-plugin-robots-txt',
+      resolve: "gatsby-plugin-robots-txt",
       options: {
-        host: 'http://allpurpose.io',
-        sitemap: 'http://allpurpose.io/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
+        host: "http://allpurpose.io",
+        sitemap: "http://allpurpose.io/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
     },
     {
       resolve: `gatsby-plugin-sass`,
@@ -59,64 +59,63 @@ module.exports = {
       },
     },
     {
-      resolve: '@prismicio/gatsby-source-prismic-graphql',
+      resolve: "@prismicio/gatsby-source-prismic-graphql",
       options: {
         repositoryName: process.env.PRISMIC_REPOSITORY_NAME,
         accessToken: process.env.PRISMIC_ACCESS_TOKEN,
         pages: [
           {
-            type: 'Page', // need capitalize
-            match: '/:uid',
-            previewPath: '/',
-            component: require.resolve('./src/templates/page.js')
+            type: "Page", // need capitalize
+            match: "/:uid",
+            previewPath: "/",
+            component: require.resolve("./src/templates/page.js"),
           },
           {
-            type: 'Team Page',
-            match: '/team',
-            previewPath: '/',
-            component: require.resolve('./src/templates/teamPage.js')
+            type: "Team Page",
+            match: "/team",
+            previewPath: "/",
+            component: require.resolve("./src/templates/teamPage.js"),
           },
           {
-            type: 'Work Page',
-            match: '/work',
-            previewPath: '/',
-            component: require.resolve('./src/templates/workPage.js')
+            type: "Work Page",
+            match: "/work",
+            previewPath: "/",
+            component: require.resolve("./src/templates/workPage.js"),
           },
           {
-            type: 'News Page',
-            match: '/news',
-            previewPath: '/',
-            component: require.resolve('./src/templates/newsPage.js')
+            type: "News Page",
+            match: "/news",
+            previewPath: "/",
+            component: require.resolve("./src/templates/newsPage.js"),
           },
           {
-            type: 'Contact Page',
-            match: '/contact',
-            previewPath: '/',
-            component: require.resolve('./src/templates/contactPage.js')
+            type: "Contact Page",
+            match: "/contact",
+            previewPath: "/",
+            component: require.resolve("./src/templates/contactPage.js"),
           },
           {
-            type: 'Case Study',
-            match: '/case-study/:uid',
-            previewPath: '/case-study',
-            sortBy: 'order_ASC',
-            component: require.resolve('./src/templates/caseStudy.js')
+            type: "Case Study",
+            match: "/case-study/:uid",
+            previewPath: "/case-study",
+            sortBy: "order_ASC",
+            component: require.resolve("./src/templates/caseStudy.js"),
           },
           {
-            type: 'News Article',
-            match: '/news/:uid',
-            previewPath: '/news',
-            sortBy: 'meta_firstPublicationDate_DESC',
-            component: require.resolve('./src/templates/newsArticle.js')
+            type: "News Article",
+            match: "/news/:uid",
+            previewPath: "/news",
+            sortBy: "meta_firstPublicationDate_DESC",
+            component: require.resolve("./src/templates/newsArticle.js"),
           },
           {
-            type: 'Careers',
-            match: '/careers',
-            previewPath: '/',
-            component: require.resolve('./src/templates/careersPage.js')
-          }
-        ]
+            type: "Careers",
+            match: "/careers",
+            previewPath: "/",
+            component: require.resolve("./src/templates/careersPage.js"),
+          },
+        ],
       },
-      
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- src/templates/careersPage.js
+++ src/templates/careersPage.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout/Layout"
-// import PageHeaderGeneral from "../components/pageHeader/PageHeaderGeneral"
-// import ContactPageContent from "../components/contactPageContent/ContactPageContent"
+import PageHeaderGeneral from "../components/pageHeader/PageHeaderGeneral"
 
 export const query = graphql`
   query CareersPageQuery {
@@ -10,9 +9,25 @@ export const query = graphql`
       allCareerss {
         edges {
           node {
+            page_description
+            page_title
             _meta {
-              uid
               type
+              uid
+            }
+            mission_statement
+            culture_photo
+            culture_photo1
+            culture_photo2
+            culture_photo3
+            mission_header
+            job_listings {
+              job_listing {
+                ... on PRISMIC_Job_listing {
+                  job_title
+                  location
+                }
+              }
             }
           }
         }
@@ -22,8 +37,7 @@ export const query = graphql`
 `
 
 const CareersPage = (props) => {
-
-  console.log(props);
+  console.log(props)
   const [selectedPalette, setSelectedPalette] = useState(null)
 
   function getRandomInt(min, max) {
@@ -40,25 +54,83 @@ const CareersPage = (props) => {
 
   const {
     _meta,
-    // page_title,
-    // page_description,
+    page_title,
+    page_description,
+    mission_header,
+    mission_statement,
+    culture_photo,
+    culture_photo1,
+    culture_photo2,
+    culture_photo3,
+    job_listings,
     // seo_title,
   } = props.data.prismic.allCareerss.edges[0].node
+  // debugger
 
   const { uid, type } = _meta
+
+  const outputJobListings = (props) => {
+    return props.data.prismic.allCareerss.edges[0].node.job_listings.map(
+      (job, index) => {
+        return (
+          <div className="col-md-3">
+            <div>
+              <p>{job_listings[index].job_listing.job_title[0].text}</p>
+              <p>{job_listings[index].job_listing.location[0].text}</p>
+            </div>
+          </div>
+        )
+      }
+    )
+  }
 
   // let seoTitle = seo_title ? seo_title : page_title
 
   return (
     <Layout palette={selectedPalette} type={type} uid={uid}>
-    {/* <Layout seoTitle={seoTitle} palette={selectedPalette} type={type} uid={uid}> */}
-      {/* <PageHeaderGeneral title={page_title} description={page_description} /> */}
-      <div className="page-sections apply-color-theme ">
+      {/* <Layout seoTitle={seoTitle} palette={selectedPalette} type={type} uid={uid}> */}
+      <PageHeaderGeneral title={page_title} description={page_description} />
+      <div className="page-sections  ">
+        <div className="container mt-48">
+          {/* <hr className="theme-color mb-48 mt-0" /> */}
+          <div className="row">
+            <div className="col-md-3">
+              <h2 className="">{mission_header[0].text}</h2>
+            </div>
+            <div className="col-md-9 ">
+              <p className="heading-01">{mission_statement[0].text}</p>
+
+              <p className="mt-20 mb-48">Browse open positions ↓</p>
+            </div>
+          </div>
+        </div>
         <div className="container">
-          <hr className="theme-color mb-48 mt-0" />
-          Careers!
-          {/* <ContactPageContent /> */}
-          <hr className="theme-color mt-48 mb-0" />
+          <div className="row">
+            <div className="col-md-6 offset-md-3">
+              <img alt={culture_photo1.alt} src={culture_photo1.url} />
+            </div>
+            <div className="col-md-3">
+              <img alt={culture_photo2.alt} src={culture_photo2.url} />
+              <img
+                alt={culture_photo.alt}
+                className="mt-4"
+                src={culture_photo.url}
+              />
+              <img
+                className="mt-4"
+                alt={culture_photo3.alt}
+                src={culture_photo3.url}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="container py-24">
+          <div className="row">
+            <div className="col-md-3">
+              <h2 className="heading-01">Open Positions</h2>
+            </div>
+            {outputJobListings(props)}
+          </div>
         </div>
       </div>
     </Layout>
