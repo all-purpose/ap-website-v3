a947bea1a3561d74b597b1969aa4daac96ef37eb
Dave <dave@Daves-MacBook-Pro-2.local>
2020-11-02 16:38:33 -0800

    uses srcset for team

--- src/components/teamMember/TeamMember.js
+++ src/components/teamMember/TeamMember.js
@@ -17,15 +17,15 @@ const TeamMember = ({ photo, name, specialties }) => {
   return (
     <div className="team-member">
       <div className="team-member-photo">
-      <picture>
+      {/* <picture>
         <source media="(max-width: 576px)" srcSet={mobileSrc} />
         <img src={photo.url} alt={photo.alt} />
-      </picture>
-        {/* <img  
-          srcSet={`${photo.url} 576w`} 
-          src={mobileSrc} 
+      </picture> */}
+        <img
+          className="lazyload"  
+          data-src={photo.url}
           alt={photo.alt} 
-        /> */}
+        />
       </div>
       <h2 className="team-member-name body-short-02 serif mt-4">{name}</h2>
       <div className="team-member-specialties body-01">
